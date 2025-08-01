33132628c7eb890c3e38dc16995f40aaafac3e8d
Ethannoma27 <Ethannoma27@hotmail.com>
2025-08-01 17:30:18 +0800

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
+// pages/index.js
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // 重定向到 public 目录下的 index.html 文件
-    router.replace('/index.html');
-  }, [router]);
-
-  // 重定向期间不渲染任何内容
-  return null;
-
-
-} 
+  return (
+    <div>
+      <h1>欢迎来到你的 Next.js 项目！</h1>
+      <p>这里是首页。</p>
+    </div>
+  );
+}  
